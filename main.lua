9aa85fc8fc063776ff84155d46be586d6df16140
Jacopo Mucci <mucci.jacopo@gmail.com>
2020-04-06 09:26:12 +0200

    Star explorer game

---
+++ main.lua
@@ -0,0 +1,16 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+local composer = require( "composer" )
+
+display.setStatusBar( display.HiddenStatusBar )
+
+math.randomseed( os.time() )
+
+audio.reserveChannels( 1 )
+audio.setVolume( 0.5, { channel = 1 } )
+
+composer.gotoScene( "menu" )
